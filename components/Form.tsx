9ec293b787d5e401745fa3fe2552f572bf2a8692
gangbeomsu <kbs21c1987@gmail.com>
2024-05-01 17:03:07 +0900

    Form 컴포넌트 추가

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,5 +1,23 @@
 const Form = () => {
-  return <form>Form</form>;
+  return (
+    <form>
+      <div className="flex flex-col gap-4">
+        <div>
+          <label htmlFor="name">이름: </label>
+          <input type="text" id="name" name="name" placeholder="이름" />
+        </div>
+        <div>
+          <label htmlFor="email">이메일: </label>
+          <input type="email" id="email" placeholder="이메일" />
+        </div>
+        <div>
+          <label htmlFor="subject">문의 내용: </label>
+          <textarea placeholder="문의 내용" id="subject" name="subject" />
+        </div>
+      </div>
+      <button type="submit">전송</button>
+    </form>
+  );
 };
 
 export default Form;
